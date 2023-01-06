a98bb20809c4b40b069882257d5ab4c98cead5e6
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-01-06 12:16:35 +0530

    Added DeviceList.js

---
+++ BackEnd/DeviceList.js
@@ -0,0 +1,9 @@
+const deviceIpData = new Map();
+deviceIpData.set("100.253.39.152", "OFFLINE");
+deviceIpData.set("100.253.39.152", "OFFLINE");
+deviceIpData.set("10.253.39.152", "OFFLINE");
+deviceIpData.set("10.253.39.190", "OFFLINE");
+
+module.exports = {
+  deviceIpData,
+};
